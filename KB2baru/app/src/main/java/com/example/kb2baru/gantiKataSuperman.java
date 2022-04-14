dc90ef1d2585a128596f4df90d1ba5ed0d3d22c8
fxchris8 <73061856+fxchris8@users.noreply.github.com>
2022-04-14 15:01:02 +0700

    Add files via upload

---
+++ KB2baru/app/src/main/java/com/example/kb2baru/gantiKataSuperman.java
@@ -0,0 +1,14 @@
+package com.example.kb2baru;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class gantiKataSuperman extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ganti_kata_superman);
+    }
+}
